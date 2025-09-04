0b4c8fa07c30eb3531b3749564d600ccd2bcf32c
nitinkumar0017 <nitin.kumar2024@nst.rishihood.edu.in>
2025-09-04 13:24:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ const allArticles = JSON.parse(fs.readFileSync('./db.json', 'utf-8'));
 
 app.get('/search', (req, res) => {
 
+
  // TODO: Implement the search and pagination logic here
+  const {name,limit=5,page =1} = req.query;
+  if(!name){
+    return res.status(400).json({"error": "Search name parameter is required."})
+  }
+  const response = allArticles.filter((item)=> item.title.toLowerCase().includes(name.toLocaleLowerCase()))
 
+  const endingIdx = (page*limit)-1
+  const startingIdx = endingIdx - (limit-1);
+  const  filterResponse = response.filter((_,idx)>= startingIdx && idx<=endingIdx)
 
+  return res.status(200).json({
+    "currentPage": page,
+    "totalPages": page.length/limit,
+    "totalResults": filterResponse.length,
+    "articles": filterResponse
+  })
 });
 
 
